4c41ea28f148da9c0222c3370aa500e61c7723ef
unknown <ana_pereira_@live.com.pt>
2018-12-01 18:18:15 +0000

    novo

--- js/custom.js
+++ js/custom.js
@@ -14,25 +14,3 @@ function openModal(nome) {
 $('.close').click(function(){
 	$('.modal').hide();
 });
-
-function myMap() {
-
-    const myLocation = new google.maps.LatLng(41.158241, -8.628995)
-
-    const mapProp = {
-        center: myLocation,
-        zoom: 18,
-		mapTypeId: google.maps.MapTypeId.HYBRID
-		styles: []
-    }
-    const map = new google.maps.Map(
-        document.getElementById("googleMap"), 
-        mapProp)
-
-        var marker = new google.maps.Marker({
-            position:myLocation,
-            animation:google.maps.Animation.BOUNCE
-            });
-          
-          marker.setMap(map);
-}
