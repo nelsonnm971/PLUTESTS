f65d8392c342519d6034a72ee80ba1de568ca66c
unknown <ana_pereira_@live.com.pt>
2018-12-02 19:02:55 +0000

    new_modalwith problems

--- js/modal.js
+++ js/modal.js
@@ -1,61 +1,57 @@
 
-function openModal(){
+function openModal(id){
+      var modalID = $('.modal_'+id);
+      modalID.show();
 
-    var $content = $('.modal_info').detach();
-  
-    $('.open_button').on('click', function(e){
-      modal.open({
-        content: $content,
-        width: 540,
-        height: 270,
-      });
-      $content.addClass('modal_content');
-      $('.modal, .modal_overlay').addClass('display');
-      $('.open_button').addClass('load');
-    });
+    $(".modal_close").click(function(e){
+      modalID.hide();
+   });
 }  
 
-var modal = (function(){
+
+
+
+// var modal = (function(){
   
-    var $close = $('<button role="button" class="modal_close" title="Close"><span></span></button>');
-    var $content = $('<div class="modal_content"/>');
-    var $modal = $('<div class="modal"/>');
-    var $window = $(window);
-  
-    $modal.append($content, $close);
-  
-    $close.on('click', function(e){
-      $('.modal, .modal_overlay').addClass('conceal');
-      $('.modal, .modal_overlay').removeClass('display');
-      $('.open_button').removeClass('load');
-      e.preventDefault();
-      modal.close();
-    });
-  
-    return {
-      center: function(){
-        var top = Math.max($window.height() - $modal.outerHeight(), 0) / 2;
-        var left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
-        $modal.css({
-          top: top + $window.scrollTop(),
-          left: left + $window.scrollLeft(),
-        });
-      },
-      open: function(settings){
-        $content.empty().append(settings.content);
-  
-        $modal.css({
-          width: settings.width || 'auto',
-          height: settings.height || 'auto'
-        }).appendTo('body');
-  
-        modal.center();
-        $(window).on('resize', modal.center);
-      },
-      close: function(){
-        $content.empty();
-        $modal.detach();
-        $(window).off('resize', modal.center);
-      }
-    };
-  }());
+//   var $close = $('<button role="button" class="modal_close" title="Close"><span></span></button>');
+//   var $content = $('<div class="modal_content"/>');
+//   var $modal = $('<div class="modal"/>');
+//   var $window = $(window);
+
+//   $modal.append($content, $close);
+
+//   $close.on('click', function(e){
+//     $('.modal, .modal_overlay').addClass('conceal');
+//     $('.modal, .modal_overlay').removeClass('display');
+//     $('.open_button').removeClass('load');
+//     e.preventDefault();
+//     modal.close();
+//   });
+
+//   return {
+//     center: function(){
+//       var top = Math.max($window.height() - $modal.outerHeight(), 0) / 2;
+//       var left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
+//       $modal.css({
+//         top: top + $window.scrollTop(),
+//         left: left + $window.scrollLeft(),
+//       });
+//     },
+//     open: function(settings){
+//       $content.empty().append(settings.content);
+
+//       $modal.css({
+//         width: settings.width || 'auto',
+//         height: settings.height || 'auto'
+//       }).appendTo('body');
+
+//       modal.center();
+//       $(window).on('resize', modal.center);
+//     },
+//     close: function(){
+//       $content.empty();
+//       $modal.detach();
+//       $(window).off('resize', modal.center);
+//     }
+//   };
+// }());
